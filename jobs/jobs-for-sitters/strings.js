e0202c0e7a69ce28ababca39ad28ee3691220d48
Tomas Halajcik <tomashalajcik@Tomass-MacBook-Pro.local>
2026-04-08 09:02:24 +0200

    Add default sitters job advert card

--- jobs/jobs-for-sitters/strings.js
+++ jobs/jobs-for-sitters/strings.js
@@ -12,6 +12,19 @@ window.STRINGS = {
     "bodyTitle": "Prototyp se připravuje",
     "bodyText": "Mobilní kontejner je připravený pro první sitter-facing obrazovky hledání práce.",
     "primaryCta": "Otevřít editor stringů",
-    "secondaryCta": "Zpět na rozcestník"
+    "secondaryCta": "Zpět na rozcestník",
+    "jobAdvert": {
+      "posted": "Posted 2 hours ago",
+      "title": "Potočná 44, Brno–Bystrc",
+      "distance": "3 km",
+      "price": "450 Kč/h",
+      "service": "Babysitting",
+      "date": "June 4, 18:00",
+      "author": "Alexandra",
+      "wrote": "wrote:",
+      "body": "Hello, I am looking for a nanny for regular babysitting from September for the next school year for two children, a son (4 years old in November) and a daughter (2 years old). Mon-Fri 8-14:00. After agreement possible adjustment of time. Exceptional evening babysitting. It is also possible to use the services of a babysitter for a joint holiday, but all by mutual agreement.",
+      "reply": "Reply to job",
+      "otherActions": "Other actions"
+    }
   }
 };
--- jobs/jobs-for-sitters/strings_en.js
+++ jobs/jobs-for-sitters/strings_en.js
@@ -15,7 +15,20 @@
       "bodyTitle": "Prototype scaffold",
       "bodyText": "The mobile container is ready for the first sitter-facing job-search screens.",
       "primaryCta": "Open strings editor",
-      "secondaryCta": "Back to hub"
+      "secondaryCta": "Back to hub",
+      "jobAdvert": {
+        "posted": "Posted 2 hours ago",
+        "title": "Potočná 44, Brno–Bystrc",
+        "distance": "3 km",
+        "price": "450 Kč/h",
+        "service": "Babysitting",
+        "date": "June 4, 18:00",
+        "author": "Alexandra",
+        "wrote": "wrote:",
+        "body": "Hello, I am looking for a nanny for regular babysitting from September for the next school year for two children, a son (4 years old in November) and a daughter (2 years old). Mon-Fri 8-14:00. After agreement possible adjustment of time. Exceptional evening babysitting. It is also possible to use the services of a babysitter for a joint holiday, but all by mutual agreement.",
+        "reply": "Reply to job",
+        "otherActions": "Other actions"
+      }
     }
   });
 })();
