a1defce3f75ff9481cfa582683b1e707606b401e
thalajcik-blip <thalajcik@gmail.com>
2026-04-13 09:02:11 +0200

    Update jobs/jobs-for-sitters/strings.js via /strings editor

--- jobs/jobs-for-sitters/strings.js
+++ jobs/jobs-for-sitters/strings.js
@@ -84,7 +84,7 @@ window.STRINGS = {
     "primaryCta": "Otevřít editor řetězců",
     "secondaryCta": "Zpět na přehled",
     "jobAdvert": {
-      "postedLabel": "zveřejnějnila",
+      "postedLabel": "zveřejnila",
       "postedTime": "před 2 hodinami",
       "title": "Potočná 44, Brno–Bystrc",
       "distance": "3 km",
